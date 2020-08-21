ad58f3a7fa9fedbde3e8025d3f7c21ea63ffe6da
Amanda Gettys <afgettys@gmail.com>
2020-08-21 15:59:59 -0400

    adding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
-CREATE burgers_db;
+CREATE DATABASE burgers_db;
 DROP DATABASE IF EXISTS burguers_db;
 USE burgers_db;
 
 CREATE  TABLE burgers (
     id AUTO_INCREMENT NOT NULL,
     burguer_name VARCHAR (30) NOT NULL,
-    devoured boolean 
-)
+    devoured boolean DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db (burger_name) VALUES ("Cheeseburguer")
+INSERT INTO burgers_db (burger_name) VALUES ("Veggieburguer")
+INSERT INTO burgers_db (burger_name) VALUES ("BaconCheeseburguer","true")
